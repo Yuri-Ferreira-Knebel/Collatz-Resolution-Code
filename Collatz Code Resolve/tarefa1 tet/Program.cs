efc7684fc6c7bbd544a3e2b49e87560acdd08078
Yuri-Ferreira-Knebel <148491340+Yuri-Ferreira-Knebel@users.noreply.github.com>
2023-10-19 16:15:22 -0300

    Add files via upload

---
+++ Collatz Code Resolve/tarefa1 tet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarefa1_tet
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            while (numero != 1)
+            {
+                if (numero % 2 == 1) // Verifica se o número é ímpar
+                {
+                    Console.WriteLine(numero);
+                    numero = numero * 3 + 1;
+                }
+                else // O número é par
+                {
+                    Console.WriteLine(numero);
+                    numero = numero / 2;
+                }
+            }
+
+            Console.WriteLine(numero);
+            Console.ReadKey();
+        }
+
+       
+    }
+
+
+}
